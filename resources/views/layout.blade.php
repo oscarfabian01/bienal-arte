a7a2b9e0e4c412a3b3642d753f12966e0f57f282
Edwar Forero <eafp.91@gmail.com>
2017-01-26 14:38:40 -0500

    se modifica favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>@yield('title') | Bienal del arte</title>
+        <link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('img/faviconbienal.png') }}" />
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
        
         <title>@yield('title') | Bienal del arte</title>
-
+        <link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('img/faviconbienal.png') }}" />
         <!-- Fonts -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
